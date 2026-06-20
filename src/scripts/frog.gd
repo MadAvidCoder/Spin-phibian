0f5a2cfd4d0317ef5dc3667568648efd129bce02
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2026-06-20 21:52:18 +1000

    Implement Grapple Created a simple grapple mechanic, based on the mouse location

--- src/scripts/frog.gd
+++ src/scripts/frog.gd
@@ -1,7 +1,23 @@
 extends CharacterBody2D
 
-enum States {ground, air, grappled}
-var state: States = States.air
+enum States {
+	GROUND,
+	AIR,
+	PULLING,
+	ORBITING
+}
+
+var state: States = States.AIR
+
+@export_category("Grappling")
+@export var orbit_radius: float = 80.0
+@export var pull_speed: float = 600.0
+@export var max_angular_speed: float = 12.0
+var angular_speed: float = 6.0
+var angle: float = 0.0
+
+
+var anchor_pos: Vector2
 
 @export_category("Platforming")
 @export var gravity: Vector2 = Vector2(0, 50)
@@ -9,7 +25,13 @@ var state: States = States.air
 const SPEED: float = 300.0
 
 func _physics_process(delta: float) -> void:
-	process_state()
+	# HACK: Temporary only
+	if Input.is_action_just_pressed("grapple"):
+		enter_grapple(get_global_mouse_position())
+	if Input.is_action_just_released("grapple"):
+		change_state(States.AIR)
+	
+	process_state(delta)
 	move_and_slide()
 
 func change_state(new_state: States):
@@ -17,28 +39,56 @@ func change_state(new_state: States):
 	state = new_state
 	enter_state()
 
+func enter_grapple(pos: Vector2):
+	anchor_pos = pos
+	change_state(States.PULLING)
+
 func enter_state():
 	match state:
-		States.air:
+		States.AIR:
 			pass
+		States.ORBITING:
+			var offset = global_position - anchor_pos
+			angle = offset.angle()
+			var tangent = Vector2(-offset.y, offset.x).normalized()
+			
+			angular_speed = velocity.dot(tangent) < orbit_radius
+			angular_speed = clamp(angular_speed, -max_angular_speed, max_angular_speed)
 
 func exit_state():
 	match state:
-		States.air:
+		States.AIR:
 			pass
+		States.ORBITING:
+			var tangent = Vector2.RIGHT.rotated(angle + PI / 2)
+			velocity = tangent * angular_speed * orbit_radius
 
-func process_state():
+func process_state(delta: float):
 	match state:
-		States.air:
+		States.AIR:
 			var input_direction = Input.get_axis("left", "right")
 			velocity.x = input_direction * SPEED
 			velocity += gravity
 			if is_on_floor():
-				change_state(States.ground)
+				change_state(States.GROUND)
 		
-		States.ground:
+		States.GROUND:
 			var input_direction = Input.get_axis("left", "right")
 			velocity.x = input_direction * SPEED
 			
 			if not is_on_floor():
-				change_state(States.air)
+				change_state(States.AIR)
+		
+		States.PULLING:
+			var to_anchor = anchor_pos - global_position
+			var dist = to_anchor.length()
+			
+			if dist <= orbit_radius:
+				change_state(States.ORBITING)
+				return
+			
+			velocity = to_anchor.normalized() * pull_speed
+		
+		States.ORBITING:
+			angle += angular_speed * delta
+			global_position = anchor_pos + Vector2.RIGHT.rotated(angle) * orbit_radius
