c5ed9ca60cfb1677d53b2cfd3129cfdc72811a71
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2026-06-25 14:40:08 +1000

    Magma anchor reset Added a timer to reset the magma anchors state after time

--- src/scripts/magma_anchor.gd
+++ src/scripts/magma_anchor.gd
@@ -1,10 +1,23 @@
 extends Anchor
 class_name MagmaAnchor
 
+@export var reset_time: float = 1.5
+
+@onready var timer = $Timer
+
 func on_grabbed():
 	super.on_grabbed()
 	sprite.play("heating")
+	timer.stop()
+
+func on_released():
+	super.on_released()
+	sprite.pause()
+	timer.start(reset_time)
 
 func _on_animation_finished():
 	if sprite.animation == "heating" and is_grabbed:
 		frog.respawn()
+
+func _on_timer_timeout():
+	sprite.play("idle")
